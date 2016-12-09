68d1259a92a93c78b94e6137aa32df550e6f1795
mattlaguardia <matthewlaguardia@gmail.com>
2016-12-08 22:20:31 -0800

    adds google sheets as db for emails

--- app/public/landing-page-assets/scripts/landing-page.js
+++ app/public/landing-page-assets/scripts/landing-page.js
@@ -13,4 +13,29 @@ jQuery(document).ready(function() {
 		$(this).find('.minutes').text(event.offset.minutes);
 		$(this).find('.seconds').text(event.offset.seconds);
 	});
+
+	$('#subscribe').on('click', function(event) {
+		event.preventDefault()
+		var email = $('#subscribe-email').val()
+
+		$.ajax({
+			url: "https://docs.google.com/forms/d/e/1FAIpQLSc6l_0OE-1NQMenajNBnmhpQeDQFP6ZDACEiWQON2pufOm68w/formResponse",
+			data: {
+				"entry.2077103770": email
+			},
+			type: "POST",
+			dataType: "xml",
+			statusCode: {
+				0: function () {
+					console.log("Switch")
+				},
+				200: function () {
+					console.log("Switch")
+				}
+			}
+		})
+	}
+
+	)
+
 });
