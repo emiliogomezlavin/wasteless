904b2e4620afc210d271d521b940493434dbefa8
mattlaguardia <matthewlaguardia@gmail.com>
2016-12-08 18:18:46 -0800

    fixes seed file

--- auth/passport.js
+++ auth/passport.js
@@ -2,17 +2,16 @@ const passport = require('passport'),
       knex = require('../models/database.js');
 
 module.exports = function () {
-  passport.serialize( function (user,done){
+  passport.serializeUser( function (user, done){
     done(null, user.id);
   });
 
-  passport.deserialize( function (id, done){
+  passport.deserializeUser( function (id, done){
     knex('users').where({id}).first()
     .then(function (user) { done(null, user);})
-    .catch(function (err) { 
+    .catch(function (err) {
       console.log(err);
       done(err, null);
     });
   })
 }
-
--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,22 +2,18 @@ const bcrypt = require('bcrypt');
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('users').del()
+  return knex.table('users').del()
     .then(function () {
       const salt = bcrypt.genSaltSync();
       const hash = bcrypt.hashSync('123456', salt);
-      return Promise.join([
-        // Inserts seed entries
-        knex('users').insert({ 
-          id: 1, 
-          username: 'benyang',
-          password: hash,
-          email: 'ben@puto.com',
-          first_name: 'Benito',
-          last_name: 'Camelo',
-          donator: false
-        })
-        
-      ]);
-    });
+      return knex('users').insert({
+        id: 1,
+        username: 'benyang',
+        password: hash,
+        email: 'ben@puto.com',
+        first_name: 'Benito',
+        last_name: 'Camelo',
+        donator: false
+      })
+  });
 };
