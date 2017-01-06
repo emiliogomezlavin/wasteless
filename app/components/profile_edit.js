770bf277aa49535224cbbda1d8dc5599a6018d5d
mattlaguardia <matthewlaguardia@gmail.com>
2017-01-06 15:36:49 -0800

    updates edit profile

--- app/components/profile_edit.js
+++ app/components/profile_edit.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 class EditProfile extends React.Component {
   render() {
+    console.log(this.state)
     return (
       <div id="edit-profile-form">
         <h3>Edit Profile</h3>
+        <p>Something {user.username}</p>
       </div>
     )
   }
