7c25e7326f73249ca5a8c067bd27ab3e621cfd43
mattlaguardia <matthewlaguardia@gmail.com>
2017-01-19 18:54:36 -0800

    psql config:

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,8 @@
-const databaseName = 'wasteless_app';
-
 module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'postgres://kigmrpflyvdecd:63d86be38192798648ecc9a64cfc3fd5221749551d159edfec872b13a2a77ca9@ec2-107-22-236-252.compute-1.amazonaws.com:5432/d26bav6nm5v4p4?ssl=true'
+      database: 'postgres://kigmrpflyvdecd:63d86be38192798648ecc9a64cfc3fd5221749551d159edfec872b13a2a77ca9@ec2-107-22-236-252.compute-1.amazonaws.com:5432/d26bav6nm5v4p4'
     },
     migrations: {
        directory: __dirname + '/db/migrations'
