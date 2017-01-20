5703468cd7eef1081058e3de9995d034a83e9814
mattlaguardia <matthewlaguardia@gmail.com>
2017-01-19 19:23:41 -0800

    heroku new

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'postgres://kigmrpflyvdecd:63d86be38192798648ecc9a64cfc3fd5221749551d159edfec872b13a2a77ca9@ec2-107-22-236-252.compute-1.amazonaws.com:5432/d26bav6nm5v4p4'
+      database: 'postgres://kigmrpflyvdecd:63d86be38192798648ecc9a64cfc3fd5221749551d159edfec872b13a2a77ca9@ec2-107-22-236-252.compute-1.amazonaws.com:5432/d26bav6nm5v4p4?ssl=true'
     },
     migrations: {
        directory: __dirname + '/db/migrations'
