33d6ddd200f9eeee8dbc3de12bdbeaa060e7f309
mattlaguardia <matthewlaguardia@gmail.com>
2017-01-19 19:26:00 -0800

    psql update

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'postgres://kigmrpflyvdecd:63d86be38192798648ecc9a64cfc3fd5221749551d159edfec872b13a2a77ca9@ec2-107-22-236-252.compute-1.amazonaws.com:5432/d26bav6nm5v4p4?ssl=true'
+      database: 'postgres://ykmtlokjudojzq:bd0835dad058d128c6fcaefcd1fa197af59a4f1a5a7e30a1d1f500dce0e38aa6@ec2-107-20-141-145.compute-1.amazonaws.com:5432/d99ludobfqlqgv?ssl=true'
     },
     migrations: {
        directory: __dirname + '/db/migrations'
