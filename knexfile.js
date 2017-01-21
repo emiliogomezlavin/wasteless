711baf770c495c32a858617edee4db2abcaa475e
Emilio Gomez Lavin <emiliogomezlavin@gmail.com>
2017-01-20 16:03:56 -0800

    all fixed for heroku deploy working

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,16 @@
 module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'wasteless_app'
+    },
+    migrations: {
+       directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+       directory: __dirname + '/db/seeds'
+    }
+  }, 
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
